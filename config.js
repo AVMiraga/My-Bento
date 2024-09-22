8d54272736e45950a78e7d07f42a9d60efd52324
Miraga <93393424+AVMiraga@users.noreply.github.com>
2024-09-22 09:44:04 +0400

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'bd5e378503939ddaee76f12ad7a97608', // Write here your API Key
+	weatherKey: 'e83b3c4c08285bf87b99f9bbc0abe3f0', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
